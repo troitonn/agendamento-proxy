e060bb46bde966f69afaf6e82a12f61f161f3a0a
troitonn <150198515+troitonn@users.noreply.github.com>
2025-09-09 20:34:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.get('/relatorio', async (req, res) => {
 
   const hoje = new Date();
   const dataFimFinal = formatarData(hoje);
-  const dataInicioFinal = formatarData(adicionarDias(hoje, -57));
+  const dataInicioFinal = formatarData(adicionarDias(hoje, -50));
 
   const payload = {
     report,
